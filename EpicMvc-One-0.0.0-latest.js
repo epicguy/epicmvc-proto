f81c66871aea0e79d39ffb240ed8dedae8a3de98
Jamie Hollowell <jamie.hollowell@dv-mobile.com>
2014-08-23 05:20:53 +0000

    JCS: Tag_page_part part=(handleIt) fix; BaseDevl-Tag-Tag_foreach debug ehnanced msg

--- EpicMvc-One-0.0.0-latest.js
+++ EpicMvc-One-0.0.0-latest.js
@@ -2701,11 +2701,12 @@
     };
 
     TagExe.prototype.Tag_page_part = function(oPt) {
-      var after, before, dynamicInfo, f, out, _ref;
+      var after, before, dynamicInfo, f, out, part, _ref;
       f = ':tag.page-part:' + oPt.attrs.part;
+      part = this.viewExe.handleIt(oPt.attrs.part);
       this.info_parts.push(this.loadPartAttrs(oPt));
       _ref = this.checkForDynamic(oPt), before = _ref[0], after = _ref[1], dynamicInfo = _ref[2];
-      out = before + (this.viewExe.includePart(this.viewExe.handleIt(oPt.attrs.part), dynamicInfo)) + after;
+      out = before + (this.viewExe.includePart(part, dynamicInfo)) + after;
       this.info_parts.pop();
       return out;
     };
@@ -4412,7 +4413,7 @@ tmpl: {
         _ref = at_table.split('/'), lh = _ref[0], rh = _ref[1];
         if (lh in this.info_foreach) {
           if (!(rh in this.info_foreach[lh].row)) {
-            throw new Error("Sub-table missing: (" + rh + ")");
+            throw new Error("Sub-table missing: (" + rh + ") in foreach table='" + lh + "/" + rh + "' (dyn:" + (this.info_foreach[lh].dyn.join(',')));
           }
           tbl = this.info_foreach[lh].row[rh];
         } else {
