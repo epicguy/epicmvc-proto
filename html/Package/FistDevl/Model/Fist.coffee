4d7d405af4abcc27124fc8ec306536bf5bb80892
Jamie Hollowell <jamie.hollowell@dv-mobile.com>
2014-10-12 20:52:39 +0000

    JCS: FistDevl/M/Fist: Rework @epic object layout for simplier access; add filters h2h, h2d, val(idation); Add keyup/focus/blur; avoid invalidateTables if only local state needs updating

--- html/Package/FistDevl/Model/Fist.coffee
+++ html/Package/FistDevl/Model/Fist.coffee
@@ -3,127 +3,165 @@
 
 # TODO One-field support - ALLOW ONE FIELD TO APPEAR AS ITS OWN FIST
 
-# Templates: Be sure to clone these
-fist_template= state: 'new', focusFieldNm: false, FIELDS: {}
-field_template= issue: false
+E.fistD2H= (field,val) -> if field.d2h then E['fistD2H$'+ field.d2h] field, val else val
+E.fistD2H$sliceIt= (field, val) -> expr= field.d2h_expr; (String val).slice expr[ 0], expr[ 1]
 
-E.fistD2H= {}
-E.fistD2H.sliceIt= (val, expr) -> (String val).slice expr[ 0], expr[ 1]
+E.fistH2H= (field,val) ->
+	val= E.fistH2H$pre val # Master pre-filter
+	val= E['fistH2H$'+ str] val for str in (field.h2h?.split /[:,]/) ? []
+	val
+E.fistH2H$pre= (val) -> val # Users can change to e.g. val.replace /[<>]/g, ''
+E.fistH2H$trim=  (val) -> (String val).trim()
+E.fistH2H$lower= (val) -> (String val).toLowerCase()
+E.fistH2H$upper= (val) -> (String val).toUpperCase()
+E.fistH2H$zero=  (val) -> val ? 0
+E.fistH2H$null=  (val) -> val ? null
+E.fistH2H$empty= (val) -> val ? ''
+E.fistH2H$digits=(val) -> val.replace /[^0-9]/g, ''
+E.fistH2H$chars=(val) -> val.replace /[^a-z]/ig, ''
+
+E.fistVAL= (field,val) ->
+	delete field.issue
+	check= true # Assume all is good
+	# Either check if empty, or check for valid value
+	if val.length is 0
+		if field.req is true # Value is empty, but required
+			check= if field.req_text
+			then ['FIELD_EMPTY_TEXT', field.nm, field.label ? field.nm, field.req_text]
+			else ['FIELD_EMPTY', field.nm, field.label ? field.nm]
+	else
+		if field.validate
+			check= E['fistVAL$'+ field.validate] field, val
+			if check is false # Failed, but use std token
+				check= 'FIELD_ISSUE'+ if field.issue_text then '_TEXT' else ''
+	if check isnt true
+		# 'check' can be a token-array or single-token (so add some extra info if needed)
+		token= check
+		if 'A' isnt E.type_oau token
+			token= [token, field.nm, field.label ? field.nm, field.issue_text]
+		field.issue= new E.Issue field.fistNm, field.nm
+		field.issue.add token[0], token.slice 1
+	return check is true
+
+E.fistVAL$test= (field, val) ->
+	re= field.validate_expr
+	re= new RegExp re if typeof re is 'string'
+	re.test val
+
+E.fistH2D= (field) -> if field.h2d then E['fistH2D$'+ field.h2d] field, field.hval else field.hval
+E.fistH2D$zero= (field,val) -> val ? 0
+
 class Fist extends E.ModelJS
 	constructor: (view_nm, options) ->
 		# E.fistDef and E.fieldDef contain combined fists info from all pacakges
 		@fist= {} # Hash by fist-name-with-row-number, of hash of field-names, holding e.g. db/html values, issues
 		super view_nm, options
-	_dbMap: (fistVal) ->
-		return if fistVal.dbMap
-		fistVal.FIELDSdb= {}
-		for fieldNm,rec of fistVal.FIELDS
-			fieldDef= E.fieldDef[ fieldNm]
-			fistVal.FIELDSdb[ fieldDef.db_nm]= rec # A pointer to same rec by fieldNm
-	_db2html: (fistDef, fistVal, dbvals) ->
-		f= '_db2html'
-		@_dbMap fistVal
-		_log2 f, fistVal.FIELDSdb, dbvals
-		for nm,val of dbvals # TODO RUN DB2HTML FROM fieldDef
-			fieldVal= fistVal.FIELDSdb[ nm]
-			fieldDef= fieldVal.fieldDef
-			fieldVal.html= if fieldDef.d2h then E.fistD2H[ fieldDef.d2h] fieldDef.d2h_expr else val
-	action: (ctx,act,p) ->
+	action: (ctx,act,p) -> # p.async=true to avoid @invalidateTables() call (avoid recursion from M/View?)
 		f= 'action:'+ act+ '-'+ p.fist+ '/'+ p.field
 		_log2 f, p
 		{r,i,m}= ctx if ctx
 		# Expect p.fist, optional p.field, optional p.row
-		if p.fist
-			fistDef= E.fistDef[ p.fist]
-			fistNm= fistDef.fistNm
-			fistNm+= ':'+ p.row if p.row?
-			fistVal= @fist[ fistNm] ? E.merge {}, fist_template
-			_log2 f, 'got p.fist', {fistDef,fistNm,fistVal}
-			if p.field
-				fieldDef= E.fieldDef[ p.field]
-				fieldNm= fieldDef.fieldNm
-				fieldVal= fistVal.FIELDS[ fieldNm]
-				_log2 f, 'Got p.field', {fieldDef,fieldNm,fieldVal}
+		fist= @_getFist p.fist, p.row
+		field= fist.ht[ p.field] if p.field
 		# Expect built-in actions: F$change
 		switch act
-			when 'F$start' # Fire up an instance of this form; ask model to populate it
-				# p.fist, p.row
-				return if fistVal.state isnt 'new'
-				db_value_hash= E[ E.appFist fistNm]().fistGetValues fistNm
-				# Move db values to html side, using db2html, then html2html
-				for nm in fistDef.FIELDS
-					fieldDef= E.fieldDef[ nm]
-					fistVal.FIELDS[ fieldDef.fieldNm]= E.merge {fieldDef}, field_template
-				@_db2html fistDef, fistVal, db_value_hash
-				fistVal.state= 'loaded'
-				@fist[ fistNm]= fistVal
-				if ctx is false# Not if called from View
-					delete @Table[ fistNm]
-				else
-					@invalidateTables [ fistNm ]
 			when 'F$keyup', 'F$change' # User has changed a field's value possibly
 				# p.val
-				if fieldVal.html isnt p.val # Update our html-value state with el.value
-					fieldVal.html= p.val
-					if fieldVal.html.length isnt 3
-						fieldVal.issue= E.Issue.Make @view_nm, 'NOT_3'
-					else
-						fieldVal.issue= false
-					@invalidateTables [ fistNm]
+				if field.hval isnt p.val # Update our html-value state with el.value
+					had_issue= field.issue
+					field.hval= p.val
+					E.fistVAL field, field.hval
+					invalidate= true if act is 'F$change' or had_issue isnt field.issue
+			when 'F$blur'
+				was_val= field.hval
+				field.hval= E.fistH2H field, field.hval # Initial cleanup
+				was_issue= E.fistVAL field, field.hval # Validate (sets/clears .issue)
+				invalidate= true if was_val isnt field.hval or was_issue
 			when 'F$focus'
-				fistVal.focusFieldNm= fieldNm
+				if fist.fnm isnt p.field
+					fist.fnm= p.field
+					invalidate= true
 			when 'F$validate' # Controller wants a fist's db values, after whole-form-validation
-				BROKEN()
+				errors= 0
+				for fieldNm, field of fist.ht
+					errors++ if E.fistVAL field, field.hval
+				if errors
+					invalidate= true
+					r.success= 'FAIL'
+					r.errors= errors
+				else
+					r.success= 'SUCCESS'
+					ans= r[ fist.nm]= {}
+					ans[ nm]= E.fistH2D field for nm,field of fist.db
 			else return super ctx, act, p
+		if invalidate
+			if p.async isnt true then @invalidateTables [ fist.rnm] else delete @Table[ fist.rnm]
+		return
 	loadTable: (tbl_nm)->
 		# A table, which is a single fist (with some number of fields), is exposed as a row for the fist, and two subtables:
 		# (a) Field - which is a hash of each field, separatly addressable i.e. &Fist/UserLogin/AuthName;
 		# (b) Control - an array of (rows of) the fields, in the order they were speced.
 		# TODO TEST ROW SUPPORT (table="Fist/UserLogin:2"
 		[ baseFistNm, row]= tbl_nm.split ':'
-		fistDef= E.fistDef[ baseFistNm]
-		fistVal= @fist[ tbl_nm]
+		fist= @_getFist baseFistNm, row
 		Field= {}
 		Control= []
 		any_req= false
-		for fieldNm,ix in fistDef.FIELDS
-			fieldDef= E.fieldDef[ fieldNm]
-			row= @_makeField fistDef, fieldDef, ix
+		for fieldNm,ix in fist.sp.FIELDS
+			field= fist.ht[ fieldNm]
+			row= @_makeField fist, field, ix, row
 			any_req= true if row.req
 			Field[ fieldNm]= [row]
 			Control.push row
 		@Table[ tbl_nm]=[ {Field: [Field], Control, any_req}]
-	_makeField: (fistDef,fieldDef,ix,row)->
+	_makeField: (fist,field,ix,row)->
 		f= '_makeField'
-		# TODO HAVE fistNm and fieldNm BE AUTO-INSERTED INTO E.fistDef/.fieldDef BY EPICCORE
-		# TODO ALLOW FIST DEF TO HAVE OTHER HASHES, SO FIELDS GOES INTO FIELDS:
-		fistVal= @fist[ fistNm= fistDef.fistNm]
-		fieldNm= fieldDef.fieldNm
-		fieldVal= fistVal?.FIELDS[ fieldNm]
-		_log2 f, {fistVal, fieldNm, fieldVal}
+		_log2 f, {fist, field, ix}
 		# TODO FIX E-IF SO WE DON'T NEED 'yes' else '' ANYMORE!
 		defaults= {
-			is_first: ix is 0, focus: fistVal?.focusFieldNm is fieldDef.fieldNm, yes_val: 'X'
-			default: '', width: '', size: '', issue: '', value: '', selected: false, name: fieldNm, fistNm
+			is_first: ix is 0, focus: fist.fnm is field.nm, yes_val: 'X'
+			default: '', width: '', size: '', issue: '', value: '', selected: false, name: field.nm
 		}
-		fl= E.merge defaults, fieldDef
+		fl= E.merge defaults, field
 		fl.yes_val=( String (fl.cdata ? '1')) if fl.type is 'yesno'
-		fl.type= (fl.type.split ':')[0]
+		[fl.type, choice_type]= fl.type.split ':'
 		fl.id= 'U'+ E.nextCounter()
-		if fieldVal # Only if we are managing an actual instance
-			fl.value= (fieldVal.html) ? fl.default
-			fl.selected= fl.type is 'yesno' and fl.value is fl.yes_val
-			fl.issue= fieldVal.issue.asTable()[0].issue if fieldVal.issue
-		else fl.value= fl.default
+		fl.value= (field.hval) ? fl.default
+		fl.selected= fl.type is 'yesno' and fl.value is fl.yes_val
+		fl.issue= field.issue.asTable()[0].issue if field.issue
 
 		if fl.type is 'radio' or fl.type is 'pulldown'
-			choices= _getChoices fistDef, fieldDef
+			choices= _getChoices choice_type, fist, field, row
 			rows= []; s= ''
 			for ix in [0...choices.options.length]
 				s= choices.values[ ix] is (String fl.value) if fieldVal
 				rows.push option: choices.options[ ix], value: choices.values[ ix], selected: s
 				fl.Choice= rows
 		fl
-
-
+	_getFist: (p_fist, p_row) ->
+		# Return fist as record
+		# 'fist' rec is: nm:fistNm, rnm:fistNm+row, row:row, st:state, sp:spec
+		#   ht:{field recs by html name}, db:{field recs by db_nm}
+		rnm= p_fist+ if p_row then ':'+ p_row else ''
+		if rnm not of @fist
+			fist= @fist[ rnm]= {rnm, nm: p_fist, row: p_row, ht: {}, db: {}, st: 'new', sp: E.fistDef[ p_fist]}
+			for fieldNm in fist.sp.FIELDS
+				field= E.merge {}, E.fieldDef[ fieldNm], nm: fieldNm, fistNm: p_fist, row: p_row
+				fist.ht[ fieldNm]= fist.db[ field.db_nm]= field # Alias by db_nm
+		else fist= @fist[ rnm]
+		if fist.st is 'new'
+			db_value_hash= E[ E.appFist p_fist]().fistGetValues p_fist, p_row
+			for nm,val of db_value_hash
+				field= fist.db[ nm]
+				field.hval= E.fistD2H field, val
+			fist.st= 'loaded'
+		return fist
+	_getChoices: (type, fist, field) ->
+		switch type
+			when 'array'
+				field.cdata # TODO IF ONE ARRAY, MAKE IT THE HASH THING
+			when 'custom'
+				E[ E.appFist fist.nm]().fistGetChoices fist.nm, field.nm, fist.row
+			else BROKEN()
+		
 E.Model.Fist$FistDevl= Fist # Public API
--- html/Package/FistDevl/Model/Fist.js
+++ html/Package/FistDevl/Model/Fist.js
@@ -2,24 +2,117 @@
 (function() {
   'use strict';
 
-  var Fist, field_template, fist_template,
+  var Fist,
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-  fist_template = {
-    state: 'new',
-    focusFieldNm: false,
-    FIELDS: {}
+  E.fistD2H = function(field, val) {
+    if (field.d2h) {
+      return E['fistD2H$' + field.d2h](field, val);
+    } else {
+      return val;
+    }
   };
 
-  field_template = {
-    issue: false
-  };
-
-  E.fistD2H = {};
-
-  E.fistD2H.sliceIt = function(val, expr) {
+  E.fistD2H$sliceIt = function(field, val) {
+    var expr;
+    expr = field.d2h_expr;
     return (String(val)).slice(expr[0], expr[1]);
+  };
+
+  E.fistH2H = function(field, val) {
+    var str, _i, _len, _ref, _ref1, _ref2;
+    val = E.fistH2H$pre(val);
+    _ref2 = (_ref = (_ref1 = field.h2h) != null ? _ref1.split(/[:,]/) : void 0) != null ? _ref : [];
+    for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
+      str = _ref2[_i];
+      val = E['fistH2H$' + str](val);
+    }
+    return val;
+  };
+
+  E.fistH2H$pre = function(val) {
+    return val;
+  };
+
+  E.fistH2H$trim = function(val) {
+    return (String(val)).trim();
+  };
+
+  E.fistH2H$lower = function(val) {
+    return (String(val)).toLowerCase();
+  };
+
+  E.fistH2H$upper = function(val) {
+    return (String(val)).toUpperCase();
+  };
+
+  E.fistH2H$zero = function(val) {
+    return val != null ? val : 0;
+  };
+
+  E.fistH2H$null = function(val) {
+    return val != null ? val : null;
+  };
+
+  E.fistH2H$empty = function(val) {
+    return val != null ? val : '';
+  };
+
+  E.fistH2H$digits = function(val) {
+    return val.replace(/[^0-9]/g, '');
+  };
+
+  E.fistH2H$chars = function(val) {
+    return val.replace(/[^a-z]/ig, '');
+  };
+
+  E.fistVAL = function(field, val) {
+    var check, token, _ref, _ref1, _ref2;
+    delete field.issue;
+    check = true;
+    if (val.length === 0) {
+      if (field.req === true) {
+        check = field.req_text ? ['FIELD_EMPTY_TEXT', field.nm, (_ref = field.label) != null ? _ref : field.nm, field.req_text] : ['FIELD_EMPTY', field.nm, (_ref1 = field.label) != null ? _ref1 : field.nm];
+      }
+    } else {
+      if (field.validate) {
+        check = E['fistVAL$' + field.validate](field, val);
+        if (check === false) {
+          check = 'FIELD_ISSUE' + (field.issue_text ? '_TEXT' : '');
+        }
+      }
+    }
+    if (check !== true) {
+      token = check;
+      if ('A' !== E.type_oau(token)) {
+        token = [token, field.nm, (_ref2 = field.label) != null ? _ref2 : field.nm, field.issue_text];
+      }
+      field.issue = new E.Issue(field.fistNm, field.nm);
+      field.issue.add(token[0], token.slice(1));
+    }
+    return check === true;
+  };
+
+  E.fistVAL$test = function(field, val) {
+    var re;
+    re = field.validate_expr;
+    if (typeof re === 'string') {
+      re = new RegExp(re);
+    }
+    return re.test(val);
+  };
+
+  E.fistH2D = function(field) {
+    if (field.h2d) {
+      return E['fistH2D$' + field.h2d](field, field.hval);
+    } else {
+      return field.hval;
+    }
+  };
+
+  E.fistH2D$zero = function(field, val) {
+    return val != null ? val : 0;
   };
 
   Fist = (function(_super) {
@@ -31,124 +124,90 @@
       Fist.__super__.constructor.call(this, view_nm, options);
     }
 
-    Fist.prototype._dbMap = function(fistVal) {
-      var fieldDef, fieldNm, rec, _ref, _results;
-      if (fistVal.dbMap) {
-        return;
-      }
-      fistVal.FIELDSdb = {};
-      _ref = fistVal.FIELDS;
-      _results = [];
-      for (fieldNm in _ref) {
-        rec = _ref[fieldNm];
-        fieldDef = E.fieldDef[fieldNm];
-        _results.push(fistVal.FIELDSdb[fieldDef.db_nm] = rec);
-      }
-      return _results;
-    };
-
-    Fist.prototype._db2html = function(fistDef, fistVal, dbvals) {
-      var f, fieldDef, fieldVal, nm, val, _results;
-      f = '_db2html';
-      this._dbMap(fistVal);
-      _log2(f, fistVal.FIELDSdb, dbvals);
-      _results = [];
-      for (nm in dbvals) {
-        val = dbvals[nm];
-        fieldVal = fistVal.FIELDSdb[nm];
-        fieldDef = fieldVal.fieldDef;
-        _results.push(fieldVal.html = fieldDef.d2h ? E.fistD2H[fieldDef.d2h](fieldDef.d2h_expr) : val);
-      }
-      return _results;
-    };
-
     Fist.prototype.action = function(ctx, act, p) {
-      var db_value_hash, f, fieldDef, fieldNm, fieldVal, fistDef, fistNm, fistVal, i, m, nm, r, _i, _len, _ref, _ref1;
+      var ans, errors, f, field, fieldNm, fist, had_issue, i, invalidate, m, nm, r, was_issue, was_val, _ref, _ref1;
       f = 'action:' + act + '-' + p.fist + '/' + p.field;
       _log2(f, p);
       if (ctx) {
         r = ctx.r, i = ctx.i, m = ctx.m;
       }
-      if (p.fist) {
-        fistDef = E.fistDef[p.fist];
-        fistNm = fistDef.fistNm;
-        if (p.row != null) {
-          fistNm += ':' + p.row;
-        }
-        fistVal = (_ref = this.fist[fistNm]) != null ? _ref : E.merge({}, fist_template);
-        _log2(f, 'got p.fist', {
-          fistDef: fistDef,
-          fistNm: fistNm,
-          fistVal: fistVal
-        });
-        if (p.field) {
-          fieldDef = E.fieldDef[p.field];
-          fieldNm = fieldDef.fieldNm;
-          fieldVal = fistVal.FIELDS[fieldNm];
-          _log2(f, 'Got p.field', {
-            fieldDef: fieldDef,
-            fieldNm: fieldNm,
-            fieldVal: fieldVal
-          });
-        }
+      fist = this._getFist(p.fist, p.row);
+      if (p.field) {
+        field = fist.ht[p.field];
       }
       switch (act) {
-        case 'F$start':
-          if (fistVal.state !== 'new') {
-            return;
-          }
-          db_value_hash = E[E.appFist(fistNm)]().fistGetValues(fistNm);
-          _ref1 = fistDef.FIELDS;
-          for (_i = 0, _len = _ref1.length; _i < _len; _i++) {
-            nm = _ref1[_i];
-            fieldDef = E.fieldDef[nm];
-            fistVal.FIELDS[fieldDef.fieldNm] = E.merge({
-              fieldDef: fieldDef
-            }, field_template);
-          }
-          this._db2html(fistDef, fistVal, db_value_hash);
-          fistVal.state = 'loaded';
-          this.fist[fistNm] = fistVal;
-          if (ctx === false) {
-            return delete this.Table[fistNm];
-          } else {
-            return this.invalidateTables([fistNm]);
-          }
-          break;
         case 'F$keyup':
         case 'F$change':
-          if (fieldVal.html !== p.val) {
-            fieldVal.html = p.val;
-            if (fieldVal.html.length !== 3) {
-              fieldVal.issue = E.Issue.Make(this.view_nm, 'NOT_3');
-            } else {
-              fieldVal.issue = false;
+          if (field.hval !== p.val) {
+            had_issue = field.issue;
+            field.hval = p.val;
+            E.fistVAL(field, field.hval);
+            if (act === 'F$change' || had_issue !== field.issue) {
+              invalidate = true;
             }
-            return this.invalidateTables([fistNm]);
+          }
+          break;
+        case 'F$blur':
+          was_val = field.hval;
+          field.hval = E.fistH2H(field, field.hval);
+          was_issue = E.fistVAL(field, field.hval);
+          if (was_val !== field.hval || was_issue) {
+            invalidate = true;
           }
           break;
         case 'F$focus':
-          return fistVal.focusFieldNm = fieldNm;
+          if (fist.fnm !== p.field) {
+            fist.fnm = p.field;
+            invalidate = true;
+          }
+          break;
         case 'F$validate':
-          return BROKEN();
+          errors = 0;
+          _ref = fist.ht;
+          for (fieldNm in _ref) {
+            field = _ref[fieldNm];
+            if (E.fistVAL(field, field.hval)) {
+              errors++;
+            }
+          }
+          if (errors) {
+            invalidate = true;
+            r.success = 'FAIL';
+            r.errors = errors;
+          } else {
+            r.success = 'SUCCESS';
+            ans = r[fist.nm] = {};
+            _ref1 = fist.db;
+            for (nm in _ref1) {
+              field = _ref1[nm];
+              ans[nm] = E.fistH2D(field);
+            }
+          }
+          break;
         default:
           return Fist.__super__.action.call(this, ctx, act, p);
+      }
+      if (invalidate) {
+        if (p.async !== true) {
+          this.invalidateTables([fist.rnm]);
+        } else {
+          delete this.Table[fist.rnm];
+        }
       }
     };
 
     Fist.prototype.loadTable = function(tbl_nm) {
-      var Control, Field, any_req, baseFistNm, fieldDef, fieldNm, fistDef, fistVal, ix, row, _i, _len, _ref, _ref1;
+      var Control, Field, any_req, baseFistNm, field, fieldNm, fist, ix, row, _i, _len, _ref, _ref1;
       _ref = tbl_nm.split(':'), baseFistNm = _ref[0], row = _ref[1];
-      fistDef = E.fistDef[baseFistNm];
-      fistVal = this.fist[tbl_nm];
+      fist = this._getFist(baseFistNm, row);
       Field = {};
       Control = [];
       any_req = false;
-      _ref1 = fistDef.FIELDS;
+      _ref1 = fist.sp.FIELDS;
       for (ix = _i = 0, _len = _ref1.length; _i < _len; ix = ++_i) {
         fieldNm = _ref1[ix];
-        fieldDef = E.fieldDef[fieldNm];
-        row = this._makeField(fistDef, fieldDef, ix);
+        field = fist.ht[fieldNm];
+        row = this._makeField(fist, field, ix, row);
         if (row.req) {
           any_req = true;
         }
@@ -164,20 +223,17 @@
       ];
     };
 
-    Fist.prototype._makeField = function(fistDef, fieldDef, ix, row) {
-      var choices, defaults, f, fieldNm, fieldVal, fistNm, fistVal, fl, rows, s, _i, _ref, _ref1, _ref2;
+    Fist.prototype._makeField = function(fist, field, ix, row) {
+      var choice_type, choices, defaults, f, fl, rows, s, _i, _ref, _ref1, _ref2, _ref3;
       f = '_makeField';
-      fistVal = this.fist[fistNm = fistDef.fistNm];
-      fieldNm = fieldDef.fieldNm;
-      fieldVal = fistVal != null ? fistVal.FIELDS[fieldNm] : void 0;
       _log2(f, {
-        fistVal: fistVal,
-        fieldNm: fieldNm,
-        fieldVal: fieldVal
+        fist: fist,
+        field: field,
+        ix: ix
       });
       defaults = {
         is_first: ix === 0,
-        focus: (fistVal != null ? fistVal.focusFieldNm : void 0) === fieldDef.fieldNm,
+        focus: fist.fnm === field.nm,
         yes_val: 'X',
         "default": '',
         width: '',
@@ -185,29 +241,24 @@
         issue: '',
         value: '',
         selected: false,
-        name: fieldNm,
-        fistNm: fistNm
+        name: field.nm
       };
-      fl = E.merge(defaults, fieldDef);
+      fl = E.merge(defaults, field);
       if (fl.type === 'yesno') {
         fl.yes_val = String((_ref = fl.cdata) != null ? _ref : '1');
       }
-      fl.type = (fl.type.split(':'))[0];
+      _ref1 = fl.type.split(':'), fl.type = _ref1[0], choice_type = _ref1[1];
       fl.id = 'U' + E.nextCounter();
-      if (fieldVal) {
-        fl.value = (_ref1 = fieldVal.html) != null ? _ref1 : fl["default"];
-        fl.selected = fl.type === 'yesno' && fl.value === fl.yes_val;
-        if (fieldVal.issue) {
-          fl.issue = fieldVal.issue.asTable()[0].issue;
-        }
-      } else {
-        fl.value = fl["default"];
+      fl.value = (_ref2 = field.hval) != null ? _ref2 : fl["default"];
+      fl.selected = fl.type === 'yesno' && fl.value === fl.yes_val;
+      if (field.issue) {
+        fl.issue = field.issue.asTable()[0].issue;
       }
       if (fl.type === 'radio' || fl.type === 'pulldown') {
-        choices = _getChoices(fistDef, fieldDef);
+        choices = _getChoices(choice_type, fist, field, row);
         rows = [];
         s = '';
-        for (ix = _i = 0, _ref2 = choices.options.length; 0 <= _ref2 ? _i < _ref2 : _i > _ref2; ix = 0 <= _ref2 ? ++_i : --_i) {
+        for (ix = _i = 0, _ref3 = choices.options.length; 0 <= _ref3 ? _i < _ref3 : _i > _ref3; ix = 0 <= _ref3 ? ++_i : --_i) {
           if (fieldVal) {
             s = choices.values[ix] === (String(fl.value));
           }
@@ -220,6 +271,55 @@
         }
       }
       return fl;
+    };
+
+    Fist.prototype._getFist = function(p_fist, p_row) {
+      var db_value_hash, field, fieldNm, fist, nm, rnm, val, _i, _len, _ref;
+      rnm = p_fist + (p_row ? ':' + p_row : '');
+      if (!(rnm in this.fist)) {
+        fist = this.fist[rnm] = {
+          rnm: rnm,
+          nm: p_fist,
+          row: p_row,
+          ht: {},
+          db: {},
+          st: 'new',
+          sp: E.fistDef[p_fist]
+        };
+        _ref = fist.sp.FIELDS;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          fieldNm = _ref[_i];
+          field = E.merge({}, E.fieldDef[fieldNm], {
+            nm: fieldNm,
+            fistNm: p_fist,
+            row: p_row
+          });
+          fist.ht[fieldNm] = fist.db[field.db_nm] = field;
+        }
+      } else {
+        fist = this.fist[rnm];
+      }
+      if (fist.st === 'new') {
+        db_value_hash = E[E.appFist(p_fist)]().fistGetValues(p_fist, p_row);
+        for (nm in db_value_hash) {
+          val = db_value_hash[nm];
+          field = fist.db[nm];
+          field.hval = E.fistD2H(field, val);
+        }
+        fist.st = 'loaded';
+      }
+      return fist;
+    };
+
+    Fist.prototype._getChoices = function(type, fist, field) {
+      switch (type) {
+        case 'array':
+          return field.cdata;
+        case 'custom':
+          return E[E.appFist(fist.nm)]().fistGetChoices(fist.nm, field.nm, fist.row);
+        default:
+          return BROKEN();
+      }
     };
 
     return Fist;
