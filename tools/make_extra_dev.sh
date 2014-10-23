3b7556fc09e871ff19aa4d1629bb4bbd0bf7bf96
Jamie Hollowell <jamie.hollowell@dv-mobile.com>
2014-10-23 03:25:40 +0000

    JCS: Another thing to include, for e.g. extra-stuff (like Proto package)

---
+++ tools/make_extra_dev.sh
@@ -0,0 +1,30 @@
+
+# Build single version of Optional stuff (extra packages) w/o uglify
+
+ECHO() {
+  echo $*
+  $*
+}
+
+EpicDir=../html
+# OneEpic=../EpicMvc-One-2.0.0-%MD5_EPICMVC%.js
+OneEpic=$EpicDir/EpicMvc-Extra-2.0.0-latest.js
+
+cat copyright.js > $OneEpic
+
+PKG=Proto
+echo "Building:    $PKG"
+./makeit-pkg2 $EpicDir/$PKG $OneEpic
+echo "Compressing: $PKG/view"
+./makeit-view $EpicDir $EpicDir $PKG >> $OneEpic
+
+#PKG=Dev
+#echo "Building:    $PKG"
+#./makeit-pkg2 $EpicDir/$PKG $OneEpic
+#echo "Compressing: $PKG/view"
+#./makeit-view $EpicDir $EpicDir $PKG >> $OneEpic
+
+#KEY=%MD5_EPICMVC%
+#FILE=$OneEpic
+#MD5=`md5sum $FILE | cut -c1-5`
+#ECHO mv $FILE `echo $FILE | awk "{print gensub(\"$KEY\",\"$MD5\",1)}"`
